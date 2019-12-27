9de34878ec428e1d03c3b4d3660cb6f641c7916b
alexandreluisbarcelos <alexandreluisbarcelos@gmail.com>
2019-12-27 16:10:24 -0300

    Adicionar arquivos de projeto.

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,41 @@
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido : Entidade
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        public virtual Usuario Usuario { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+        public int FormaPagamentoId { get; set; }
+        public virtual FormaPagamento FormaPagamento { get; set; }
+
+        /// <summary>
+        /// Pedido deve ter pelo menos um item de pedido ou muitos itens de pedido
+        /// </summary>
+        public virtual ICollection<ItemPedido> ItensPedido { get; set; }
+
+        public override void Validate()
+        {
+            if(!ItensPedido.Any())
+                AdicionarCritica("Crítica - Pedido não pode ficar sem item de pedido.");
+
+            if (string.IsNullOrEmpty(CEP))
+                AdicionarCritica("Crítica - CEP deve estar peenchido.");
+
+            if (FormaPagamentoId == 0)
+                AdicionarCritica("Crítica - Não foi informado a forma de pagamenmto.");
+        }
+    }
+}
