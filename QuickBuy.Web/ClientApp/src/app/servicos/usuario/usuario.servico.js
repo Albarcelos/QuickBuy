24995b596ab3c571238c3141edce1c980b0b459a
alexandreluisbarcelos <alexandreluisbarcelos@gmail.com>
2020-01-17 18:15:05 -0300

    Aula 98

---
+++ QuickBuy.Web/ClientApp/src/app/servicos/usuario/usuario.servico.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var UsuarioSevico = /** @class */ (function () {
+    function UsuarioSevico() {
+    }
+    return UsuarioSevico;
+}());
+exports.UsuarioSevico = UsuarioSevico;
+//# sourceMappingURL=usuario.servico.js.map
--- QuickBuy.Web/ClientApp/src/app/servicos/usuario/usuario.servico.ts
+++ QuickBuy.Web/ClientApp/src/app/servicos/usuario/usuario.servico.ts
@@ -1,1 +1,7 @@
-﻿
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@Angular/common/http';
+import { Observable } from 'rxjs';
+
+export class UsuarioSevico{
+
+}
