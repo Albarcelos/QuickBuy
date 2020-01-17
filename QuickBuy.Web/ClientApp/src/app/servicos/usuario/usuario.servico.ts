7c3467c5aeeede4a4ae7ab438d4e478b973d185c
alexandreluisbarcelos <alexandreluisbarcelos@gmail.com>
2020-01-17 18:29:50 -0300

    Aula 100

--- QuickBuy.Web/ClientApp/src/app/servicos/usuario/usuario.servico.ts
+++ QuickBuy.Web/ClientApp/src/app/servicos/usuario/usuario.servico.ts
@@ -1,11 +1,30 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@Angular/common/http';
 import { Observable } from 'rxjs';
+import { Usuario } from '../../modelo/usuario';
 
 @Injectable({
   providedIn: "root"
 })
 
 export class UsuarioSevico{
+
+  private BaseURL: string;
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.BaseURL = baseUrl;
+  }
+
+  public verificarUsuario(usuario: Usuario): Observable<Usuario> {
+
+    const header = new HttpHeaders().set('content-type', 'application/json');
+
+    var body = {
+      email: usuario.email,
+      senha: usuario.senha
+    }
+
+    //this.baseURL = raiz do site que pode ser por exemplo.: http://www.quickbuy.com/
+    return this.http.post<Usuario>(this.BaseURL + "api/usuario", body, { headers });
+  }
 
 }
