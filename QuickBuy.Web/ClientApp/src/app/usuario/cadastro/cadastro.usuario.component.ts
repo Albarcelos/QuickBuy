4c6dea2032d76ce5d5416923c8a1d188dce74c74
alexandreluisbarcelos <alexandreluisbarcelos@gmail.com>
2020-01-20 20:03:44 -0300

    Aula 114.

---
+++ QuickBuy.Web/ClientApp/src/app/usuario/cadastro/cadastro.usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-cadastroUsuario',
+  templateUrl: './cadastro.usuario.component.html',
+  styleUrls: ['./cadastro.usuario.component.css']
+})
+
+export class CadastroUsuarioComponent {
+
+}
